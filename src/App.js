fb978428ee8a4cf99931de9f31174790cc65fbeb
Mohammad Ahtasham <bsef17a007@pucit.edu.pk>
2020-04-13 18:47:55 +0500

    Components with card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import {Navbar,NavbarBrand} from 'reactstrap'
 import Menu from './components/MenuComponent'
+import {DISHES} from './shared/dishes'
 
+ class App extends React.Component {
 
-function App() {
+  constructor(props) {
+    super(props);
+    this.state={
+      dishes:DISHES
+    }
+  }
+
+  render()
+  {
   return (
     <div className="App">
       <Navbar dark color="primary">
@@ -11,10 +21,11 @@ function App() {
         <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu/>
+      <Menu dishes={this.state.dishes}/>
 
     </div>
   );
+}
 }
 
 export default App;
