c2c1e2b17acd578c0972dfcac1aeb1beacda585b
Mohammad Ahtasham <bsef17a007@pucit.edu.pk>
2020-05-15 00:48:26 +0500

    REACT ANIMATIONS

--- src/components/DishDetailsComponent.js
+++ src/components/DishDetailsComponent.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 import {Control,LocalForm,Errors} from "react-redux-form"
 import {Loading} from "./LoadingComponent"
 import {baseUrl} from "../shared/baseUrl"
-
+import {FadeTransform,Fade,Stagger} from "react-animation-components"
 
 const required=(val)=>val && val.length;
 const maxLength=(len)=>(val)=>!(val) ||(val.length<=len);
@@ -66,6 +66,10 @@ const minLength=(len)=>(val)=>(val) &&(val.length>=len);
     function RenderDish({dish})
     {
         return (
+            <FadeTransform in 
+            transformProps={
+                {exitTransoform:"scale{0.5} tranlateY{-50%}"}
+                }>
         <Card>
             <CardImg width='100%' src={baseUrl+dish.image} alt={dish.name} />
             <CardBody>
@@ -73,6 +77,7 @@ const minLength=(len)=>(val)=>(val) &&(val.length>=len);
             <CardText>{dish.description}</CardText>
             </CardBody>
         </Card>
+        </FadeTransform>
         );
     }
     function RenderComments({comments,postComment,dishId})
@@ -85,16 +90,21 @@ const minLength=(len)=>(val)=>(val) &&(val.length>=len);
             var d=new Date(c.date);
             var toDisplay=months[d.getMonth()]+' '+d.getDate()+','+d.getFullYear();
             return(
+                <Fade in>
             <li key={c.id}>{c.comment}<br/><br/> 
             --{c.author}, {toDisplay}<br/></li>
+            </Fade>
             )
         })
         return (
             <div>
         <h4>Comments</h4>
         <ul className='list-unstyled'>
+            <Stagger in >
             {com}
+            </Stagger>
             </ul>
+            
             <CommentForm  postComment={postComment} dishId={dishId} />
         </div>
         );
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {Card,CardImg,CardText,CardBody,CardTitle,CardSubtitle} from "reactstrap"
 import {Loading} from "./LoadingComponent"
 import {baseUrl} from "../shared/baseUrl"
+import {FadeTransform} from "react-animation-components"
+
 function RenderCard ({item,isLoading,errMess})
 {
     if(isLoading)
@@ -19,6 +21,10 @@ function RenderCard ({item,isLoading,errMess})
     else
     {
     return (
+        <FadeTransform in 
+        transformProps={
+            {exitTransoform:"scale{0.5} tranlateY{-50%}"}
+            }>
         <Card>
             <CardImg src={baseUrl+item.image} alt={item.name}/>
                 <CardBody>
@@ -27,6 +33,7 @@ function RenderCard ({item,isLoading,errMess})
                     <CardText>{item.description}</CardText>
                 </CardBody>
         </Card>
+        </FadeTransform>
     );
     }
 }
