6dc770aae493c7d79361769f5699212bbc03c096
Mohammad Ahtasham <bsef17a007@pucit.edu.pk>
2020-06-02 18:24:55 +0500

    Final Commit

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,13 +1,13 @@
 import React,{Component} from 'react';
 import {Breadcrumb, BreadcrumbItem ,Label,Row, Col,Button} from 'reactstrap'
-import {Control,Form,Errors,actions} from "react-redux-form"
+import {Control,Form,Errors} from "react-redux-form"
 import {Link} from 'react-router-dom'
 
 const required=(val)=>val && val.length;
 const maxLength=(len)=>(val)=>!(val) ||(val.length<=len);
 const minLength=(len)=>(val)=>(val) &&(val.length>=len);
 const isNumber=(val)=>!isNaN(Number(val));
-const validEmail=(val)=>/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z{2-4}]$/i.test(val);
+const validEmail=(val)=>/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]$/i.test(val);
 
 class Contact extends Component {
 
